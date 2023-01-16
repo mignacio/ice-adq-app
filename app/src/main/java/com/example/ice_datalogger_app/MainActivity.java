3d92f228f8bd39506b9b0185c78814a3123462e7
Ignacio Moya <moyaignacioj@gmail.com>
2023-01-16 20:04:28 -0300

    Back up de codigo android antes de probar biblioteca serial.

--- app/src/main/java/com/example/ice_datalogger_app/MainActivity.java
+++ app/src/main/java/com/example/ice_datalogger_app/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.ice_datalogger_app;
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Build;
@@ -12,13 +15,13 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.viewpager2.widget.ViewPager2;
 
 import com.androidplot.xy.CatmullRomInterpolator;
 import com.androidplot.xy.LineAndPointFormatter;
@@ -27,17 +30,24 @@ import com.androidplot.xy.SimpleXYSeries;
 import com.androidplot.xy.XYGraphWidget;
 import com.androidplot.xy.XYPlot;
 import com.androidplot.xy.XYSeries;
-import com.google.android.material.tabs.TabLayout;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.UUID;
 
 import java.util.Arrays;
 import java.util.Set;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     //Bluetooth globals
-    Button buttonOn, buttonOff, buttonShow;
+    TextView textView;
+    Button buttonOn, buttonOff, buttonShow, buttonScan;
     BluetoothAdapter myBluetoothAdapter;
-    ListView listView;
+    ListView listView, scanListView;
+    ArrayList<String> stringArrayList = new ArrayList<String>();
+    ArrayAdapter<String> arrayAdapter;
 
     Intent btEnablingIntent;
     int requestCodeForeEnable;
@@ -51,26 +61,36 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_DENIED)
-        {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
-            {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_DENIED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
                 ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.BLUETOOTH_CONNECT}, 2);
             }
         }
 
-        // Bluetooth stuff
+//        // Bluetooth stuff
+//        textView = (TextView) findViewById(R.id.textView);
+//        Thread2 thread2 = new Thread2();
+//        thread2.start();
+
         buttonOn = (Button) findViewById(R.id.btOn);
         buttonOff = (Button) findViewById(R.id.btOff);
         buttonShow = (Button) findViewById(R.id.btShow);
+        buttonScan = (Button) findViewById(R.id.btScan);
+        scanListView = (ListView) findViewById(R.id.scanList);
         myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         btEnablingIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
         requestCodeForeEnable = 1;
         bluetoothOnMethod();
         bluetoothOffMethod();
-
-        listView = (ListView) findViewById(R.id.listView);
         exeButton();
+        bluetoothScanMethod();
+
+        IntentFilter intentFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(myReceiver, intentFilter);
+
+        arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, stringArrayList);
+        scanListView.setAdapter(arrayAdapter);
+        //listView = (ListView) findViewById(R.id.listView);
 
         //Plot stuff
         myPlot = (XYPlot) findViewById(R.id.xyplot);
@@ -101,6 +121,48 @@ public class MainActivity extends AppCompatActivity{
         PanZoom.attach(myPlot);
     }
 
+//    private class Thread2 extends Thread {
+//        public void run() {
+//            for (int i = 0; i < 50; i++) {
+//                textView.setText(String.valueOf(i));
+//                try {
+//                    sleep(500);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//    }
+
+    BroadcastReceiver myReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(), "BLUETOOTH_CONNECT permission missing.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                stringArrayList.add(device.getName());
+                arrayAdapter.notifyDataSetChanged();
+            }
+        }
+    };
+
+    private void bluetoothScanMethod() {
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(), "BLUETOOTH_CONNECT permission missing.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                myBluetoothAdapter.startDiscovery();
+
+            }
+        });
+    }
     private void exeButton() {
         buttonShow.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -109,17 +171,17 @@ public class MainActivity extends AppCompatActivity{
                     Toast.makeText(getApplicationContext(), "BLUETOOTH_CONNECT permission missing.", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                Set<BluetoothDevice> btBondedDevices = myBluetoothAdapter.getBondedDevices();
-                String[] strings = new String[btBondedDevices.size()];
-                int index = 0;
-                if(btBondedDevices.size() > 0){
-                    for(BluetoothDevice btDevice:btBondedDevices){
-                        strings[index] = btDevice.getName();
-                        index++;
-                    }
-                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, strings);
-                    listView.setAdapter(arrayAdapter);
-                }
+//                Set<BluetoothDevice> btBondedDevices = myBluetoothAdapter.getBondedDevices();
+//                String[] strings = new String[btBondedDevices.size()];
+//                int index = 0;
+//                if(btBondedDevices.size() > 0){
+//                    for(BluetoothDevice btDevice:btBondedDevices){
+//                        strings[index] = btDevice.getName();
+//                        index++;
+//                    }
+//                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, strings);
+//                    listView.setAdapter(arrayAdapter);
+//                }
             }
         });
     }
